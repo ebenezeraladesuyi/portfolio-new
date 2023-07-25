e70eff22fd0ddcd18dcd034f92bc63dcc012144c
finbarr12 <finarrogbonna@gmail.com>
2023-07-25 19:30:20 +0100

    working

--- src/Blocks/Header.tsx
+++ src/Blocks/Header.tsx
@@ -1,5 +1,24 @@
+import styled from "styled-components";
+
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <Container>
+      <Wrapper></Wrapper>
+    </Container>
+  );
 };
 
 export default Header;
+
+const Container = styled.div`
+  width: 100%;
+  height: 60px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+const Wrapper = styled.div`
+  width: 90%;
+  height: 100%;
+  background-color: gold;
+`;
