15c0ff973dcc2fac935dead1089818b5f0d005a9
finbarr12 <finarrogbonna@gmail.com>
2023-08-03 00:56:13 +0100

    footerdone

--- src/Blocks/Footer.tsx
+++ src/Blocks/Footer.tsx
@@ -1,16 +1,30 @@
-// import {AiFillGithub} from "react-icons"
+import { AiFillGithub, AiFillLinkedin } from "react-icons/ai";
+import { BsFacebook } from "react-icons/bs";
 
 import styled from "styled-components";
 
 const Footer = () => {
   return (
     <Container>
-      &copy; {new Date().getFullYear()} Finbarr. Designed by Ogbonna Finbarr
+      <Hold>
+        <AiFillGithub size={25} style={{ marginRight: "20px" }} />
+        <AiFillLinkedin size={25} style={{ marginRight: "20px" }} />
+        <BsFacebook size={25} />
+      </Hold>
+      <p>
+        {" "}
+        &copy; {new Date().getFullYear()} Finbarr. Designed by Ogbonna Finbarr
+      </p>
     </Container>
   );
 };
 
 export default Footer;
+
+const Hold = styled.div`
+  display: flex;
+  margin-bottom: 15px;
+`;
 
 const Container = styled.div`
   width: 100%;
@@ -18,5 +32,11 @@ const Container = styled.div`
   align-items: center;
   justify-content: center;
   flex-direction: column;
-  margin-top: 20px;
+  margin-top: 70px;
+
+  p {
+    margin-top: 0;
+    display: flex;
+    align-items: center;
+  }
 `;
